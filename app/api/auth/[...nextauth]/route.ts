b2f13b77fb08e6c77061841ab4ba6e2bc524fca9
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-02-14 16:12:14 -0500

    replace Worldcoin provider with Auth0 in NextAuth configuration and remove WorldID verification widget from SignIn component

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,15 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
+import Auth0 from "next-auth/providers/auth0";
 
 const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
-
   providers: [
-    {
-      id: "worldcoin",
-      name: "Worldcoin",
-      type: "oauth",
-      wellKnown: "https://id.worldcoin.org/.well-known/openid-configuration",
-      authorization: { params: { scope: "openid" } },
-      clientId: process.env.WLD_CLIENT_ID,
-      clientSecret: process.env.WLD_CLIENT_SECRET,
-      idToken: true,
-      checks: ["state", "nonce", "pkce"],
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.sub,
-          verificationLevel:
-            profile["https://id.worldcoin.org/v1"].verification_level,
-        };
-      },
-    },
+    Auth0({
+      clientId: process.env.AUTH0_CLIENT_ID ?? "",
+      clientSecret: process.env.AUTH0_CLIENT_SECRET ?? "",
+      issuer: process.env.AUTH0_DOMAIN,
+    }),
   ],
-  callbacks: {
-    async signIn({ user }) {
-      return true;
-    },
-  },
   debug: process.env.NODE_ENV === "development",
 };
 
