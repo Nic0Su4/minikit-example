a137c49b3ab233a7840c4a96014a054c0739cb2f
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-02-18 16:59:11 -0500

    fix: update user check in Home component to ensure PayBlock renders correctly

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
       <VerifyBlock />
-      {MiniKit.user?.username ? <PayBlock /> : <WalletAuthBlock />}
+      {MiniKit.user ? <PayBlock /> : <WalletAuthBlock />}
     </main>
   );
 }
