f75d11ebb16c3f6df18d06259fc93b2c672e1f16
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-02-19 02:34:35 -0500

    fix: log BACKEND_PRIVATE_KEY in Home component for debugging purposes

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import WalletAuthBlock from "@/components/WalletAuthBlock";
 import { MiniKit } from "@worldcoin/minikit-js";
 
 export default function Home() {
+  console.log(process.env.BACKEND_PRIVATE_KEY);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
       <VerifyBlock />
