d01484ab7aacbba557f089fb6ce15f678164855b
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-02-20 16:44:12 -0500

    fix: use optional chaining to safely access username in PayBlock rendering

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
       {/* <VerifyBlock /> */}
       <WalletAuthBlock />
-      {user.username && <PayBlock />}
+      {user?.username && <PayBlock />}
     </main>
   );
 }
