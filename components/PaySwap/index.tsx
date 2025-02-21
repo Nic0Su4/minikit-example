1a6fb0d03e1d342fbe74d422a3768577c479ec86
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-02-21 11:23:35 -0500

    fix: update receiver address and adjust payment amount in PayBlock

--- components/PaySwap/index.tsx
+++ components/PaySwap/index.tsx
@@ -8,7 +8,7 @@ import {
   tokenToDecimals,
 } from "@worldcoin/minikit-js";
 
-const RECEIVER_ADDRESS = "0x669e29E89328B5D1627731b82fD503287971D358";
+const RECEIVER_ADDRESS = "0x4f2f92cf7c8d18f440bad599b4e9615838433220";
 
 export const PayBlock = () => {
   const [status, setStatus] = useState("");
@@ -76,23 +76,23 @@ export const PayBlock = () => {
       if (!confirmData.success) {
         throw new Error("Confirmación de pago fallida.");
       }
-      setStatus("Pago confirmado. Ejecutando swap...");
+      // setStatus("Pago confirmado. Ejecutando swap...");
 
-      const swapRes = await fetch("/api/perform-swap", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          fromAddress: RECEIVER_ADDRESS,
-          fromAmount: tokenToDecimals(tokenWLD, Tokens.WLD).toString(),
-        }),
-      });
-      const swapData = await swapRes.json();
-      if (!swapData.success) {
-        throw new Error("Swap fallido: " + swapData.error);
-      }
-      setStatus(
-        "Swap completado exitosamente. Transacción: " + swapData.transactionHash
-      );
+      // const swapRes = await fetch("/api/perform-swap", {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify({
+      //     fromAddress: RECEIVER_ADDRESS,
+      //     fromAmount: tokenToDecimals(tokenWLD, Tokens.WLD).toString(),
+      //   }),
+      // });
+      // const swapData = await swapRes.json();
+      // if (!swapData.success) {
+      //   throw new Error("Swap fallido: " + swapData.error);
+      // }
+      // setStatus(
+      //   "Swap completado exitosamente. Transacción: " + swapData.transactionHash
+      // );
     } catch (error: any) {
       console.error("Error en el pago:", error);
       setError(error.message || "Error desconocido");
@@ -106,7 +106,7 @@ export const PayBlock = () => {
     <div>
       <button
         onClick={() => {
-          sendPayment(2);
+          sendPayment(0.1);
         }}
         disabled={isProcessing}
       >
