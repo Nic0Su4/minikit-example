3d9485da0774322566a7d836801080b546016987
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-02-26 17:49:05 -0500

    fix: update fetchConversionRate to correctly calculate conversion rates and clean up unused code

--- components/ConversionRate/index.tsx
+++ components/ConversionRate/index.tsx
@@ -7,21 +7,27 @@ const fetchConversionRate = async () => {
     const coinResponse = await fetch(
       "https://www.worldcoinindex.com/apiservice/ticker?key=02L4vsaiNfZRP7B9a9FxZWg1RxC1gtrQXa8&label=wldbtc&fiat=usd"
     );
-    const coinData = await coinResponse.json();
-    console.log(coinData);
+    const { Markets } = await coinResponse.json();
+    console.log(Markets);
 
-    // const fxResponse = await fetch(
-    //   "https://api.exchangerate-api.com/v4/latest/USD"
-    // );
-    // const fxData = await fxResponse.json();
-    // const usdToPen = fxData?.rates?.PEN;
-    // if (!usdToPen) {
-    //   throw new Error("Error obteniendo tasa USD a PEN");
-    // }
+    const wldPriceUSD = Markets[0].Price;
 
-    // const wldPricePEN = wldPriceUSD * usdToPen;
-    // const penToWldRate = 1 / wldPricePEN;
-    // return penToWldRate;
+    const fxResponse = await fetch(
+      "https://api.exchangerate-api.com/v4/latest/USD"
+    );
+    const fxData = await fxResponse.json();
+    const usdToPen = fxData?.rates?.PEN;
+    if (!usdToPen) {
+      throw new Error("Error obteniendo tasa USD a PEN");
+    }
+
+    const usdToPenSell = usdToPen + 0.1;
+
+    console.log(usdToPenSell);
+
+    const wldPricePEN = wldPriceUSD * usdToPenSell;
+    const penToWldRate = 1 / wldPricePEN;
+    return penToWldRate;
   } catch (error) {
     console.error(error);
     return null;
@@ -29,30 +35,27 @@ const fetchConversionRate = async () => {
 };
 
 export default function ConversionRateComponent() {
-  const [conversionRate, setConversionRate] = useState(null);
+  const [conversionRate, setConversionRate] = useState<number | null>(null);
 
   useEffect(() => {
-    fetchConversionRate();
-
-    // const getRate = async () => {
-    //   const rate = await fetchConversionRate();
-    //   setConversionRate(rate);
-    // };
-    // getRate();
-    // const interval = setInterval(() => {
-    //   getRate();
-    // }, 10000);
-    // return () => clearInterval(interval);
+    const getRate = async () => {
+      const rate = await fetchConversionRate();
+      setConversionRate(rate);
+    };
+    getRate();
+    const interval = setInterval(() => {
+      getRate();
+    }, 10000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div>
-      {/* {conversionRate !== null ? (
+      {conversionRate !== null ? (
         <p>Tasa de conversión: 1 PEN = {conversionRate.toFixed(6)} WLD</p>
       ) : (
         <p>Cargando tasa de conversión...</p>
-      )} */}
-      <p>hola</p>
+      )}
     </div>
   );
 }
