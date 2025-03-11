60dee4ed699bee4633b3a800e7b1b8075e3a77c2
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-03-11 10:13:11 -0500

    refactor: remove unused product fetching logic and Supabase client from StorePage component

--- app/(auth)/store/[id]/page.tsx
+++ app/(auth)/store/[id]/page.tsx
@@ -2,42 +2,17 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { useFetchProducts } from "@/hooks/useFetchProducts";
 import ProductCard from "@/components/User/Products/ProductCard";
 import CartSummary from "@/components/User/Products/CartSummary";
-import { createClient } from "@/utils/supabase/client";
 import { ArrowLeft } from "lucide-react";
 
 export default function StorePage({ params }: { params: { id: string } }) {
-  const { products, fetchProducts } = useFetchProducts();
   const [cart, setCart] = useState<{ id: number; quantity: number }[]>([]);
   const [storeName, setStoreName] = useState<string | null>(null);
-  const [searchTerm, setSearchTerm] = useState("");
   const router = useRouter();
 
   useEffect(() => {
     setCart([]);
-    fetchProducts(params.id);
-  }, [params.id, fetchProducts]);
-
-  useEffect(() => {
-    const fetchStoreName = async () => {
-      const supabase = createClient();
-      const { data, error } = await supabase
-        .from("tiendas")
-        .select("*")
-        .eq("id", +params.id)
-        .single();
-
-      if (error) {
-        console.error("Error al obtener el nombre de la tienda:", error);
-      } else {
-        setStoreName(data?.nombre || "Tienda desconocida");
-        console.log(data);
-      }
-    };
-
-    fetchStoreName();
   }, [params.id]);
 
   const addToCart = (productId: number) => {
@@ -56,7 +31,7 @@ export default function StorePage({ params }: { params: { id: string } }) {
 
   return (
     <div className="min-h-screen bg-background">
-      <header className="sticky top-0 z-10 bg-background border-b p-4 flex items-center justify-between">
+      {/* <header className="sticky top-0 z-10 bg-background border-b p-4 flex items-center justify-between">
         <button onClick={() => router.back()} className="flex items-center">
           <ArrowLeft className="mr-2 h-4 w-4" />
           Volver
@@ -64,7 +39,7 @@ export default function StorePage({ params }: { params: { id: string } }) {
         <h1 className="text-xl font-bold">
           {storeName ? storeName : "Cargando..."}
         </h1>
-        <div /> {/* Espaciador */}
+        <div />
       </header>
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
@@ -87,7 +62,7 @@ export default function StorePage({ params }: { params: { id: string } }) {
             onCheckout={() => router.push("/checkout")}
           />
         )}
-      </div>
+      </div> */}
     </div>
   );
 }
