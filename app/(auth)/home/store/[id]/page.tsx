cdf9f61022c473dd477fb3c55bd4254d674dbd82
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-03-13 20:02:37 -0500

    fix: remove unnecessary max-width constraint from StorePage container

--- app/(auth)/home/store/[id]/page.tsx
+++ app/(auth)/home/store/[id]/page.tsx
@@ -26,7 +26,7 @@ export default async function StorePage({
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <div className="container mx-auto px-4 py-6 max-w-80">
+      <div className="container mx-auto px-4 py-6">
         <StoreHeader store={store} categories={categories} />
         <ProductsSection items={items} />
       </div>
