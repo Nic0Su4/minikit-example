9df23055a292b55903a754be7c42e508e111c6a2
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-03-13 20:44:50 -0500

    fix: add new hostname for RoboHash in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "https://kbswiwnnsqgxhcdzrfvt.supabase.co/"
       },
+      {
+        protocol: "https",
+        hostname: "https://robohash.org/"
+      },
     ]
   }
 };
